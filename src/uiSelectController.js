2023be92d64eb8d9f398e9f710ccd31c1b5ca49a
Jason Gray <jason@geckolabs.co.uk>
2016-10-11 16:49:57 +0100

    fix(_updateItemLocked) properly unlock locked items when lock conditions change
    Previously when the lock expression value changes from true to false the locked value was not removed from the locked list.

--- src/uiSelectController.js
+++ src/uiSelectController.js
@@ -423,7 +423,7 @@ uis.controller('uiSelectCtrl',
             ctrl.close(skipFocusser);
             return;
           }
-        }        
+        }
         _resetSearchInput();
         $scope.$broadcast('uis:select', item);
 
@@ -499,7 +499,7 @@ uis.controller('uiSelectCtrl',
         }
 
       if (!isLocked && lockedItemIndex > -1) {
-        lockedItems.splice(lockedItemIndex, 0);
+        lockedItems.splice(lockedItemIndex, 1);
       }
     }
 
